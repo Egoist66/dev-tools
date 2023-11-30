ca68af73eee76d63538eb4c94338f9e3b3966891
Farid Mahmudov <Razormad666@gmail.com>
2023-11-30 12:25:10 +0300

    memo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,12 @@
-import { FC, ReactNode, useCallback, useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap'
-import { Form } from "react-bootstrap";
+import { FC, useCallback, useEffect, useState } from 'react';
 import { selector, useAppDispatch, useAppSelector } from '../store/store';
 import { setEntityMode } from '../store/entities-slice/entities-reducer';
 import { ArrayControls } from './ArrayControls';
 import { ObjectControls } from './ObjectControls';
 import { ClassControls } from './ClassControls';
+import { AppView } from './AppView';
 
-const entityOptions = [
+export const entityOptions = [
 
   { id: '1', entity: 'Array' },
   { id: '2', entity: 'Object' },
@@ -28,7 +27,7 @@ export const App: FC = () => {
 
   }, [_mode])
 
-  const showControls = () => {
+  const showControls = useCallback(() => {
 
     switch (globalMode.mode) {
       case '1':
@@ -41,59 +40,25 @@ export const App: FC = () => {
       default:
         return 'No such value!'
     }
-  }
+  }, [globalMode.mode])
 
   useEffect(() => {
     dispatch(setEntityMode({ mode: _mode }))
   }, [_mode])
 
   return (
+
     <AppView
       _mode={_mode}
       showControls={showControls}
       changeMode={changeMode}
     />
+
   )
 }
 
 
 
-type AppViewProps = {
-  changeMode: (mode: string) => void
-  showControls: () => ReactNode 
-  _mode: string
-
-}
-
-const AppView: FC<AppViewProps> = ({ changeMode, _mode, showControls}) => {
-  return (
-    <Container className='p-5'>
-
-      <h1 className='display-6 text-center'>Entities Constructor</h1>
-
-      <Row>
-        <Col>
-          <Form.Select data-value={_mode} value={_mode} onChange={(e) => changeMode(e.currentTarget.value)}>
-            <option>Choose what entity to create</option>
 
 
-            {entityOptions.map(o => (
-              <option key={o.id} value={o.id}>{o.entity}</option>
-            ))}
 
-          </Form.Select>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col>
-              
-          {showControls()} 
-
-        </Col>
-      </Row>
-
-
-    </Container>
-  )
-}
